76038b5e91150b505d86edc69e0aef234030825b
pll <723848199@qq.com>
2023-09-04 23:03:22 +0800

    redoc_denlgu

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from core.server import server
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-
 app = server.create_app()
 
-app.mount("/", StaticFiles(directory=r"E:\bug_track\vue"), name="dist")
+app.mount("/", StaticFiles(directory=r"/Users/pll/Code/bug_track/vue"), name="dist")
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -18,30 +17,36 @@ from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory=r"E:\bug_track\vue")
+app.mount('/static', StaticFiles(directory=r"/Users/pll/Code/bug_track/vue/static"), name="static")
+templates = Jinja2Templates(directory=r"/Users/pll/Code/bug_track/vue")
 security = HTTPBasic()
+
+
 @app.get("/")
 def login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
-@app.post("/login")
-def login(credentials: HTTPBasicCredentials = security):
-    if credentials.username == "admin" and credentials.password == "password":
-        return templates.TemplateResponse("login.html")
-    else:
-        return {"Login": "Failed"}
+@app.get("/login",summary=" 登录")
+def login(request: Request):
+                # (credentials: HTTPBasicCredentials = security):
+    # if credentials.username == "admin" and credentials.password == "password":
+    # return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse(r"/html/denglu.html", {"request": request})
+    # else:
+    #     return {"Login": "Failed"}
 
 
 # 事件监听
 app.add_event_handler('startup', startup(app))
 app.add_event_handler('shutdown', stopping(app))
 
+
 async def demo():
     # user = await User.all()
     # print(user)
     return '123'
+
 
 # 运行app
 if __name__ == '__main__':
