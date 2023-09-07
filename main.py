6dd7dd9ebeb9e82c49bd0ea10d59ad1d4b792843
all <all>
2023-09-07 10:03:50 +0800

    与前端VUE联动

--- main.py
+++ main.py
@@ -63,6 +63,20 @@ def login(request: Request):
 app.add_event_handler('startup', startup(app))
 app.add_event_handler('shutdown', stopping(app))
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def demo():
     # user = await User.all()
